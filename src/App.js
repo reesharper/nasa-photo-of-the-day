bad2d0d809382fc232ccd9409b3f27987f842885
reesharper <reestimharper@gmail.com>
2020-10-08 01:24:48 -0400

    added current date function

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import "./App.css";
 import axios from 'axios'
 import Header from './Header'
 import Container from './Container'
+import getFormattedDate from "./Date";
+
+
 
 function App() {
 
   const [picture, setPicture] = useState({});
-  const [date, setDate] = useState("2020-10-07");
+  const [date, setDate] = useState(getFormattedDate());
 
   useEffect(() => {
     const fetchPics = () => {
       axios.get(`https://api.nasa.gov/planetary/apod?api_key=LTFmZ7vh9t42vCfsBrWqJdNXH4tUsOR6AOkMcBqY&date=${date}`)
       .then(res => {
-        // console.log(res);
-        // console.log(res.data);
         setPicture(res.data)
       })
       .catch(err => {
